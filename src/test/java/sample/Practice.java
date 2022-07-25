0ec55988567cd1b5a8d5672d444a0c9550c378ff
Zabi2305 <105333882+Zabi2305@users.noreply.github.com>
2022-07-25 17:22:48 +0530

    Update Practice.java

--- src/test/java/sample/Practice.java
+++ src/test/java/sample/Practice.java
@@ -4,8 +4,8 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("hi ");
 		System.out.println("hello");
-		System.out.println("byeee");
-		//updates
+		System.out.println("This world is beautyful");
+		
 		
 		
 		
